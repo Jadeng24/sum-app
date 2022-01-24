a1543abd00ef9dbb455df98e11a013bd88249b86
jaden <jaden.goodwin24@gmail.com>
2022-01-23 23:18:30 -0700

    adding server changes and dummy data

--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const port = process.env.PORT || 8080;
 
+// place holder for the data
+const users = ['test'];
+
+app.use(bodyParser.json());
+
+app.get('/api/users', (req, res) => {
+	console.log('api/users called!!!!');
+	res.json(users);
+});
+
+app.post('/api/user', (req, res) => {
+	const user = req.body.user;
+	console.log('Adding user::::::::', user);
+	users.push(user);
+	res.json('user addedd');
+});
+
+// For Production (prod)
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('build'));
 	app.get('*', (req, res) => {
 		req.sendFile(path.resove(__dirname, 'build', '../build/index.html'));
+	});
+} else {
+	app.get('/', (req, res) => {
+		res.send('App Works !!!!');
 	});
 }
 
--- src/pages/Boat-Builder/Boat-Builder.tsx
+++ src/pages/Boat-Builder/Boat-Builder.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Boat-Builder.scss';
 const BoatBuilder = () => {
-	return <div className="Boat-builder">Boat Builder</div>;
+	return <div className="Boat-builder">Boat Builders</div>;
 };
 
 export default BoatBuilder;
