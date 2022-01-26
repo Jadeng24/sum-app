3873cf4a33e2a39e9d95fc3aa45c3dfdf96f3b1c
jaden <jaden.goodwin24@gmail.com>
2022-01-25 23:43:26 -0700

    Adding modal for login and register

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LoginModal from '../Login-Modal/Login-Modal';
 
 import NavLogo from './Nav-logo/Nav-logo';
 
 import './Nav.scss';
 
 const Nav = () => {
+	const [modalIsOpen, setIsOpen] = React.useState(false);
+
+	const openModal = () => {
+		setIsOpen(true);
+	};
+	const afterOpenModal = () => {
+		// references are now sync'd and can be accessed.
+		console.log('modal is opened');
+	};
+	const closeModal = () => {
+		setIsOpen(false);
+	};
+
 	return (
 		<nav>
 			<NavLogo />
@@ -17,6 +31,16 @@ const Nav = () => {
 				<Link className="build-link" to="/boat-builder">
 					Build
 				</Link>
+
+				<div>
+					<button onClick={openModal}>Open Modal</button>
+					{/* // modal  */}
+					<LoginModal
+						modalIsOpen={modalIsOpen}
+						afterOpenModal={afterOpenModal}
+						closeModal={closeModal}
+					/>
+				</div>
 			</div>
 		</nav>
 	);
