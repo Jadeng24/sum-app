ebd814ab38aa418d2b0c6946ed92071f49cee1ff
jaden <jaden.goodwin24@gmail.com>
2022-01-30 21:02:56 -0700

    a much of lint crap

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const pool = require('../db');
+// eslint-disable-next-line import/extensions
+const pool = require('../db.ts');
 require('dotenv').config();
 
 const port = process.env.PORT || 8080;
