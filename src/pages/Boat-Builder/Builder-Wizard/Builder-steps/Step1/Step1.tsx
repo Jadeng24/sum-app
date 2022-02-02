6bc23da9a0c7e6f47a205c6d1d877b77a7818004
jaden <jaden.goodwin24@gmail.com>
2022-02-01 23:50:26 -0700

    restructuring boats to be mapped dynamically

--- src/pages/Boat-Builder/Builder-Wizard/Builder-steps/Step1/Step1.tsx
+++ src/pages/Boat-Builder/Builder-Wizard/Builder-steps/Step1/Step1.tsx
@@ -3,8 +3,9 @@
 /* eslint-disable react/jsx-curly-brace-presence */
 /* eslint-disable import/no-unresolved */
 import React, { useEffect, useState } from 'react';
-import Carousel from 'react-spring-3d-carousel';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Virtual } from 'swiper';
 import 'swiper/css/bundle';
 
 import aluminumBoat from '../../../../../assets/boat.png';
@@ -16,18 +17,62 @@ import BoatSlide from './Boat-Slide/Boat-Slide';
 const Step1 = () => {
     const [swiperRef, setSwiperRef] = useState(null);
     const slideTo = (eventObj) => {
-        console.log(eventObj);
         if (eventObj.isPrev) {
             swiperRef.slideTo(swiperRef.activeIndex - 1);
         }
-        // swiperRef.slideTo(index - 1, 0);
     };
+    const boats = [
+        {
+            boatImg: aluminumBoat,
+            title: 'Aluminum Jetboat',
+            length: 10,
+            model: 'aluminum',
+            id: 1,
+        },
+        {
+            boatImg: aluminumBoat,
+            title: 'Aluminum Jetboat',
+            length: 12,
+            model: 'aluminum',
+            id: 2,
+        },
+        {
+            boatImg: aluminumBoat,
+            title: 'Aluminum Jetboat',
+            length: 14,
+            model: 'aluminum',
+            id: 3,
+        },
+        {
+            boatImg: inflatableBoat,
+            title: 'Inflatable Race boat',
+            length: 10,
+            model: 'inflatable',
+            id: 1,
+        },
+        {
+            boatImg: inflatableBoat,
+            title: 'Inflatable Race boat',
+            length: 12,
+            model: 'inflatable',
+            id: 2,
+        },
+        {
+            boatImg: inflatableBoat,
+            title: 'Inflatable Race boat',
+            length: 14,
+            model: 'inflatable',
+            id: 3,
+        },
+    ];
 
     return (
         <div className="wizardStep carousel-holder flex">
             <div className="boat-floor" />
+
             <Swiper
                 onSwiper={setSwiperRef}
+                modules={[Virtual]}
                 initialSlide={0}
                 spaceBetween={10}
                 slidesPerView={3}
@@ -37,55 +82,16 @@ const Step1 = () => {
                 slideToClickedSlide
                 centeredSlides
             >
-                <SwiperSlide>
-                    <BoatSlide
-                        boatImg={aluminumBoat}
-                        title="Aluminum Jetboat"
-                        length={10}
-                        model="aluminum"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <BoatSlide
-                        boatImg={aluminumBoat}
-                        title="Aluminum Jetboat"
-                        length={12}
-                        model="aluminum"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <BoatSlide
-                        boatImg={aluminumBoat}
-                        title="Aluminum Jetboat"
-                        length={14}
-                        model="aluminum"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <BoatSlide
-                        boatImg={inflatableBoat}
-                        title="Inflatable Raceboat"
-                        length={10}
-                        model="inflatable"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <BoatSlide
-                        boatImg={inflatableBoat}
-                        title="Inflatable Raceboat"
-                        length={12}
-                        model="inflatable"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <BoatSlide
-                        boatImg={inflatableBoat}
-                        title="Inflatable Raceboat"
-                        length={14}
-                        model="inflatable"
-                    />
-                </SwiperSlide>
-                ƒ
+                {boats.map((boat, index) => (
+                    <SwiperSlide key={boat.id} virtualIndex={index}>
+                        <BoatSlide
+                            boatImg={boat.boatImg}
+                            title={boat.title}
+                            length={boat.length}
+                            model={boat.model}
+                        />
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </div>
     );
