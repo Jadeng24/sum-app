0281e967b42fce9454ec99a153b3f7964d1ab3c7
jaden <jaden.goodwin24@gmail.com>
2022-02-03 18:46:51 -0700

    removing json

--- server/server.js
+++ server/server.js
@@ -80,6 +80,7 @@ app.post('/api/contact', (req, res) => {
 // Production --------------------------------------------------------------------------
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
+    app.use(express.json);
     app.get('*', (req, res) => {
         req.sendFile(path.resolve(__dirname, 'build', '../build/index.html'));
     });
