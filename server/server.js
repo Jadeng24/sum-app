c74454febcf2fd8160d32f8ce18b5ee63071eea9
jaden <jaden.goodwin24@gmail.com>
2022-02-03 17:53:20 -0700

    adding nodemailer and contact form!

--- server/server.js
+++ server/server.js
@@ -2,15 +2,18 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 // eslint-disable-next-line import/extensions
 const pool = require('../db.ts');
 require('dotenv').config();
 
 const port = process.env.PORT || 8080;
 
+// Endpoints --------------------------------------------------------------------------
 // place holder for the data
 const users = ['test'];
 
@@ -27,7 +30,7 @@ app.post('/api/user', (req, res) => {
     res.json('user addedd');
 });
 
-app.get('/boats', async (req, res) => {
+app.get('/api/boats', async (req, res) => {
     try {
         const allBoats = await pool.query('SELECT * FROM boats');
 
@@ -37,16 +40,52 @@ app.get('/boats', async (req, res) => {
     }
 });
 
-// For Production (prod)
+// Nodemailer --------------------------------------------------------------------------
+const contactEmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.REACT_APP_GMAIL_USER,
+        pass: process.env.REACT_APP_GMAIL_PASS,
+    },
+});
+
+contactEmail.verify((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Ready to Send');
+    }
+});
+
+app.post('/api/contact', (req, res) => {
+    const { name, email, message } = req.body;
+
+    const mail = {
+        from: name,
+        to: '***************@gmail.com',
+        subject: 'Contact Form Submission',
+        html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Message: ${message}</p>`,
+    };
+    contactEmail.sendMail(mail, (error) => {
+        if (error) {
+            res.json({ status: 'ERROR' });
+        } else {
+            res.json({ status: 'Message Sent' });
+        }
+    });
+});
+
+// Production --------------------------------------------------------------------------
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
         req.sendFile(path.resolve(__dirname, 'build', '../build/index.html'));
     });
 } else {
-    app.get('/', (req, res) => {
-        res.send('App Works !!!!');
-    });
+    app.use(express.json);
+    app.use('/', router);
 }
 
 app.listen(port, (err) => {
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 
 const Contact = () => {
+    const [status, setStatus] = useState('Submit');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus('Sending...');
+        const { name, email, message } = e.target.elements;
+        const details = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
+        };
+        const response = await fetch('/api/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(details),
+        });
+        setStatus('Submit');
+        const result = await response.json();
+        alert(result.status);
+    };
     return (
-        <div className="Page Contact">
-            <h2>Contact Sport Utility Marine</h2>
+        <div className="Page Contact column">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" required />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" required />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea id="message" required />
+                </div>
+                <button type="submit">{status}</button>
+            </form>
         </div>
     );
 };
