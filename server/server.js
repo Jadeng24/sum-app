c7ee172a1f3bbacddae3d0b61ff364200a0155fb
jaden <jaden.goodwin24@gmail.com>
2022-02-05 23:49:57 -0700

    pushing expressjson

--- server/server.js
+++ server/server.js
@@ -109,6 +109,7 @@ app.post('/api/contact', (req, res) => {
 // Production --------------------------------------------------------------------------
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
+    app.use(express.json);
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', '../build/index.html'));
     });
