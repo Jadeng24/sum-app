6ef5672819fbc63e24b175e7a5e23036e6d8202f
jaden <jaden.goodwin24@gmail.com>
2022-02-09 23:57:25 -0700

    adding admin link

---
+++ src/pages/Admin/Admin.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from '../../components/Banner/Banner';
+
+const Admin = () => {
+    return (
+        <div className="Page Admin">
+            <Banner
+                title="Admin Dashboard"
+                subtitle="Manage all website-content and products"
+            />
+        </div>
+    );
+};
+
+export default Admin;
