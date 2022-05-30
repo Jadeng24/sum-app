1bcb2e0863e52130a72aa0a539f949c81b04ab76
jaden <jaden.goodwin24@gmail.com>
2022-05-29 20:07:17 -0600

    adding loader fade for boats slides

--- src/pages/BoatBuilder/BuilderWizard/BuilderSteps/Step2/Step2.tsx
+++ src/pages/BoatBuilder/BuilderWizard/BuilderSteps/Step2/Step2.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
+/* eslint-disable react/jsx-no-useless-fragment */
+import React, { useEffect, useState } from 'react';
 import { isTabletOrMobile } from '../../../../../components/Devices/MediaQuery';
 import { Boat } from '../../../../../types/Types';
+import StepLoader from '../../../StepLoader/StepLoader';
 import StepHeader from '../../BuilderComponents/StepHeader/StepHeader';
 
 import './Step2.scss';
@@ -11,52 +13,67 @@ interface Step2Props {
 
 const Step2 = (props: Step2Props) => {
     const { boat } = props;
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         console.log(boat);
+
+        setIsLoading(true);
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
     }, [boat]);
+
     return (
-        <div
-            className={`wizardStep step-content 
+        // <div>
+        <>
+            {isLoading && <StepLoader />}
+            <div
+                className={`wizardStep step-content 
             ${
                 isTabletOrMobile()
                     ? 'column isTabletOrMobile'
                     : 'row space-between'
             }`}
-        >
-            <div className="boat-preview flex">
-                {boat && (
-                    <img
-                        className="boat-image"
-                        src={boat.featured_image}
-                        alt={boat.name}
+            >
+                <div className="boat-preview flex">
+                    {boat && (
+                        <img
+                            className="boat-image"
+                            src={boat.featured_image}
+                            alt={boat.name}
+                        />
+                    )}
+                </div>
+                <div className="options-wrapper column">
+                    <StepHeader
+                        title={`Build your ${boat.name}`}
+                        step="Exterior"
                     />
-                )}
+                    <div>option 1</div>
+                    <div>option 2</div>
+                    <div>option 3</div>
+                    <div>option 4</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                    <div>option 1</div>
+                </div>
             </div>
-            <div className="options-wrapper column">
-                <StepHeader title={`Build your ${boat.name}`} step="Exterior" />
-                <div>option 1</div>
-                <div>option 2</div>
-                <div>option 3</div>
-                <div>option 4</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-                <div>option 1</div>
-            </div>
-        </div>
+        </>
+        // </div>
     );
 };
 
